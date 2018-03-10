a7c429d95a015a1194b174507c7964ff6355b152
Tyler Frasca <tfrasc@gmail.com>
2018-03-09 17:56:51 -0800

    Added slash command to pay users

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('#user_id').val(Math.floor(Math.random() * 100000));
-
   // $('.choice').each(function() {
   //   var ticker = $(this).data('ticker');
   //
@@ -24,10 +23,30 @@ $(document).ready(function() {
           message: function(message) {
               // console.log("New Message!!", message);
               if(message.userMetadata.user_id == $('#user_id').val()) {
-
-                $(`.channel-content[data-id='${message.channel}']`).append(`<div class="message-container"><div class="message mine">${message.message}</div></div>`);
+                if(message.userMetadata.payment == true) {
+                  $(`.channel-content[data-id='${message.channel}']`).append(`<div class="message-container"><div class="message mine">\
+                    You paid ${message.userMetadata.amount} ${message.userMetadata.crypto} to User ${message.userMetadata.to_user}\
+                    </div></div>`
+                  );
+                } else {
+                  $(`.channel-content[data-id='${message.channel}']`).append(`<div class="message-container"><div class="message mine">${message.message}</div></div>`);
+                }
               } else {
-                $(`.channel-content[data-id='${message.channel}']`).append(`<div class="message-container"><div class="message">${message.message}</div></div>`);
+                if(message.userMetadata.payment == true) {
+                  if(message.userMetadata.payment == true && $('#user_id').val() == message.userMetadata.to_user.toString()) {
+                    $(`.channel-content[data-id='${message.channel}']`).append(`<div class="message-container"><div class="message mine">\
+                      User ${message.userMetadata.user_id} paid ${message.userMetadata.amount} ${message.userMetadata.crypto} to You\
+                      </div></div>`
+                    );
+                  } else {
+                    $(`.channel-content[data-id='${message.channel}']`).append(`<div class="message-container"><div class="message mine">\
+                      User ${message.userMetadata.user_id} paid ${message.userMetadata.amount} ${message.userMetadata.crypto} to User ${message.userMetadata.to_user}\
+                      </div></div>`
+                    );
+                  }
+                } else {
+                  $(`.channel-content[data-id='${message.channel}']`).append(`<div class="message-container"><div class="message">${message.message}</div></div>`);
+                }
               }
               $(`.channel-content[data-id='${message.channel}']`).scrollTop($(`.channel-content[data-id='${message.channel}']`)[0].scrollHeight);
           },
@@ -46,17 +65,42 @@ $(document).ready(function() {
   $(document).on('click', '.send-img', function() {
     var msg = $(this).prev().val();
     var channel = $(this).parent().prev().data('id');
+    var crypto, amount, user;
 
-    var publishConfig = {
-      channel : channel,
-      message : msg,
-      meta : {
-        user_id: $('#user_id').val()
+    if(msg.indexOf('/pay') !== 1){
+      crypto = msg.split('/')[2];
+      amount = msg.split('/')[3];
+      user = msg.split('/')[4];
+
+      var publishConfig = {
+        channel : channel,
+        message : msg,
+        meta : {
+          payment: true,
+          user_id: $('#user_id').val(),
+          to_user: user,
+          amount: amount,
+          crypto: crypto
+        }
       }
+
+      console.log(crypto, amount, user);
+      pubnub.publish(publishConfig, function(status, response) {
+          // console.log(status, response);
+      });
+    } else {
+      var publishConfig = {
+        channel : channel,
+        message : msg,
+        meta : {
+          payment: false,
+          user_id: $('#user_id').val()
+        }
+      }
+      pubnub.publish(publishConfig, function(status, response) {
+          // console.log(status, response);
+      });
     }
-    pubnub.publish(publishConfig, function(status, response) {
-        // console.log(status, response);
-    });
 
     $(this).prev().val('');
   });
